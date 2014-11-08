ef3cd9cd4815457fc39816e731309ae205b60e45
Sean <slevy@mediasignage.com>
2014-11-08 15:43:49 -0800

    working on micro web server

--- dev/test_human.js
+++ dev/test_human.js
@@ -1,50 +1,60 @@
 var firmata = require('firmata');
-var app = require('express');
+var express = require("express");
+var app = express();
+
+var HUMAN = 3;
+var LEDPIN = 13;
+var serialReady = false;
 
 var board = new firmata.Board("/dev/ttyATH0", function (err) {
 
     console.log('Firmata connected');
-
+    serialReady = true;
     if (err) {
         console.log(err);
         board.reset();
         return;
     }
-
-    function getTime(){
-        var t = new Date();
-        return t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds();
-    }
-
-    var HUMAN = 3;
     board.pinMode(HUMAN, board.MODES.INPUT);
+    board.pinMode(LEDPIN, board.MODES.OUTPUT);
     board.digitalRead(HUMAN, function (e) {
         if (e == 1)
             console.log('detected ' + getTime());
     });
 });
 
+// curl 192.168.1.94:5000/mcu13:1
+// curl 192.168.1.94:5000/webapp
 
-var express = require("express");
-var app = express();
+app.all("/mcu:pin", function (req, res) {
+    if (!serialReady) {
+        res.send("NOTREADY");
+        return;
+    }
+    var pin = (req.params.pin).split(':')[0];
+    var state = (req.params.pin).split(':')[1];
 
-/* serves main page */
-app.get("/", function(req, res) {
+    console.log('setting pin ' + pin + ' ' + state);
+
+    if (state == 1) {
+        board.digitalWrite(pin, board.HIGH);
+    } else {
+        board.digitalWrite(pin, board.LOW);
+    }
+    res.send("OK");
+
+});
+
+app.all("/webapp", function (req, res) {
     res.sendfile('index.htm')
 });
 
-app.post("/user/add", function(req, res) {
-    /* some server side logic */
-    res.send("OK");
-});
-
-/* serves all the static files */
-app.get(/^(.+)$/, function(req, res){
-    console.log('static file request : ' + req.params);
-    res.sendfile( __dirname + req.params[0]);
-});
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Listening on " + port);
 });
+
+function getTime() {
+    var t = new Date();
+    return t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds();
+}
