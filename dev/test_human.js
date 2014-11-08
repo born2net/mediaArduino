1ce24a10c7ae558719dcb80ebcc974c1d99b2ad4
Sean <slevy@mediasignage.com>
2014-11-08 12:22:37 -0800

    added express

--- dev/test_human.js
+++ dev/test_human.js
@@ -1,4 +1,5 @@
 var firmata = require('firmata');
+var app = require('express');
 
 var board = new firmata.Board("/dev/ttyATH0", function (err) {
 
@@ -21,4 +22,29 @@ var board = new firmata.Board("/dev/ttyATH0", function (err) {
         if (e == 1)
             console.log('detected ' + getTime());
     });
+});
+
+
+var express = require("express");
+var app = express();
+
+/* serves main page */
+app.get("/", function(req, res) {
+    res.sendfile('index.htm')
+});
+
+app.post("/user/add", function(req, res) {
+    /* some server side logic */
+    res.send("OK");
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res){
+    console.log('static file request : ' + req.params);
+    res.sendfile( __dirname + req.params[0]);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
 });
